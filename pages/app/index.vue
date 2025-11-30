4a14756c0fe602d04293f2d2b7344835f56dfbc9
BjoernMuenker <mail@bjoernmuenker.de>
2025-12-01 00:44:15 +0100

    new time test

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -22,9 +22,9 @@
     layout: 'app',
   });
 
-  const { data: serverTime } = await useAsyncData('serverTime', () => getServerTime());
-  const { data: scores } = await useAsyncData('score', () => getScore(user.value?.user_metadata.display_name ?? ''));
-  const { data: savegames } = await useAsyncData('savegames', async () => {
+  const { data: serverTime } = useAsyncData('serverTime', () => getServerTime());
+  const { data: scores } = useAsyncData('score', () => getScore(user.value?.user_metadata.display_name ?? ''));
+  const { data: savegames } = useAsyncData('savegames', async () => {
     const savegames = await getSavegames({ userId: user.value?.sub ?? '', sortBy: 'updated_at' });
     if (!savegames || savegames.length === 0) return [];
 
@@ -137,6 +137,7 @@
     <!-- {{ useDebug().getCompletedPuzzleCount() }}
     {{ useDebug().getMissingItemsCount() }}
     {{ useDebug().getDuplicatedItemGroups() }} -->
+    serverTime: {{ new Date(serverTime) }} / {{ new Date(puzzles[0].unlocksAt) }}
     <div class="puzzles" ref="sliderRef">
       <div class="slide" v-for="(puzzle, index) in puzzles" :key="puzzle.id" :class="{ active: activeSlideIndex === index }">
         <button
